55c4660d27b0ba262233a5cf6cbc030fae2c796c
Michael Wood <michael.wood@opendataservices.coop>
2024-02-29 16:11:48 +0000

    dokku: Add dokku specific configs and reqs

--- registry/settings/settings.py
+++ registry/settings/settings.py
@@ -29,7 +29,7 @@ SECRET_KEY = get_random_string(50, "abcdefghijklmnopqrstuvwxyz0123456789!@#%^&*(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["registry.threesixtygiving.org"]
 
 
 # Application definition
